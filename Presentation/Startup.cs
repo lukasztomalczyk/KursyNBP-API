7b161be34efb162dd949bfa9a4dcb97c6bef9b10
Lukasz Tomalczyk <lukasz.tomalczyk@onet.pl>
2019-01-04 13:07:04 +0100

    work on bootstrap

--- Presentation/Startup.cs
+++ Presentation/Startup.cs
@@ -29,12 +29,6 @@ namespace kursyNBP
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
             services.Configure<Urls>(Configuration.GetSection("NBP"));
             services.AddDependency();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
@@ -55,7 +49,6 @@ namespace kursyNBP
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseCookiePolicy();
 
             app.UseMvc(routes =>
             {
