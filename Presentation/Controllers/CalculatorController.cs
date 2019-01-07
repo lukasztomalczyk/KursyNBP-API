6c24ee708b524b891d20b5cf62adb08f414af5dd
Lukasz Tomalczyk <lukasz.tomalczyk@onet.pl>
2019-01-07 13:36:25 +0100

    dodaj calculator contriller

---
+++ Presentation/Controllers/CalculatorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["cookie"] = "cal";
+            return View();
+        }
+    }
+}
